ffa4880f3b69a0eafc208ddb61319a46fc28504f
happy-mei <may.air@qq.com>
2025-07-27 01:20:51 +1200

    feat: Input System and Screen bounds detection

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BirdScript : MonoBehaviour
 {
@@ -9,12 +8,38 @@ public class BirdScript : MonoBehaviour
     public float flapStrength = 10;
     public LogicScript logic;
     public bool birdIsAlive = true;
+
     // Flap Tail Logic
     public GameObject tail;
     private SpriteRenderer tailRenderer;
     public Sprite[] tailSprites; // Array of tail sprites
     private float resetDelay = 0.2f; // Delay before changing tail back
     private Coroutine tailResetCoroutine;
+
+    private BirdControls controls; // Input system controls
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+        controls = new BirdControls();
+        controls.Gameplay.Flap.performed += ctx => OnFlap();
+    }
+
+    private void OnEnable()
+    {
+        if (controls != null)
+        {
+            controls.Gameplay.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (controls != null)
+        {
+            controls.Gameplay.Disable();
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -32,17 +57,49 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0) && birdIsAlive)
+        // Check if the bird is within screen bounds
+        float margin = Camera.main.orthographicSize * 0.2f; // 20% of screen height
+        float topBound = Camera.main.orthographicSize + margin;
+        float bottomBound = -Camera.main.orthographicSize - margin;
+
+        float birdY = transform.position.y;
+        if (birdY < bottomBound || birdY > topBound)
         {
-            myRigidBody.velocity = Vector2.up * flapStrength;
-            ChangeTailSprite(0); // Change to upward sprite
-            // Cancel any existing coroutine before starting a new one
-            if (tailResetCoroutine != null)
-            {
-                StopCoroutine(tailResetCoroutine);
-            }
-            tailResetCoroutine = StartCoroutine(ResetTailSprite());
+            birdIsAlive = false;
+            logic.gameOver();
         }
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     if ((Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0) && birdIsAlive)
+    //     {
+    //         myRigidBody.velocity = Vector2.up * flapStrength;
+    //         ChangeTailSprite(0); // Change to upward sprite
+    //         // Cancel any existing coroutine before starting a new one
+    //         if (tailResetCoroutine != null)
+    //         {
+    //             StopCoroutine(tailResetCoroutine);
+    //         }
+    //         tailResetCoroutine = StartCoroutine(ResetTailSprite());
+    //     }
+    // }
+
+    private void OnFlap()
+    {
+        Debug.Log("onflap called");
+
+        if (!birdIsAlive) return;
+
+        myRigidBody.velocity = Vector2.up * flapStrength;
+        ChangeTailSprite(0);
+
+        if (tailResetCoroutine != null)
+        {
+            StopCoroutine(tailResetCoroutine);
+        }
+        tailResetCoroutine = StartCoroutine(ResetTailSprite());
     }
 
     private IEnumerator ResetTailSprite()
